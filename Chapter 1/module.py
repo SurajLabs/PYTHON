68d84b16d8a9b90bd46769ec98fd2e5516534fc9
Suraj <roysun011@gmail.com>
2026-08-10 16:52:27 +0530

    Add Chapter 1

---
+++ Chapter 1/module.py
@@ -0,0 +1,10 @@
+# install pyjokes using pip install pyjokes
+import pyjokes
+
+print("Printing Jokes... ")
+# this print a random jokes
+joke = pyjokes.get_joke()
+print(joke)
+
+# so thanks all of you 
+# that is my program
