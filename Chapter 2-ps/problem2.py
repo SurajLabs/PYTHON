701cf267559ab139f2a5b39152239b1d9af9de0d
Suraj <roysun011@gmail.com>
2026-08-11 21:43:38 +0530

    Add Chapter 2 Problem Sets

---
+++ Chapter 2-ps/problem2.py
@@ -0,0 +1,3 @@
+a = 80
+b = 9
+print("Remainder when a is divided by b is", a % b)
---
+++ Chapter 2-ps/problem6.py
@@ -0,0 +1,5 @@
+a = int(input("Enter your number: "))
+
+print("Square of a number is", a*a)
+print("Cube of a number is", a**3) #if we want to add exponent then we use double star (**)
+# print("Cube of a number is", a^3) INVALID
